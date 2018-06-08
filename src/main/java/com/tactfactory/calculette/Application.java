56dd995dbae03869f082132236059d008caf08bd
Aurélien Le Pévédic <aurelien.le.pevedic@gmail.com>
2018-06-08 09:48:14 +0200

    Commit#1 Aurélien - project init with classes and gradle initiated

---
+++ src/main/java/com/tactfactory/calculette/Application.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright 2018 AURELIEN.
+ ******************************************************************************/
+package com.tactfactory.calculette;
+
+/**
+ * Mon application.
+ * @version 1.0
+ * @author Aurélien Le Pévédic
+ *
+ */
+public final class Application{        
+
+    private Application() {
+        super();
+    }
+
+    /**
+     * Entry-point of application.
+     * @param args Argument from CLI.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello world !!");
+    }
+}
